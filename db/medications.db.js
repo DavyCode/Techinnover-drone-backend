87215ebfd84a7c5ca47df4583361747881fc68eb
David Azemoh Paul <david@thriveagric.com>
2023-06-30 23:10:28 +0100

    feat: fetch all medications

---
+++ db/medications.db.js
@@ -0,0 +1,25 @@
+const Medications = [
+  {
+    _id: "203c3b9e945c49-02b9e944f3c3",
+    name: 'Melatonin',
+    code: 'MEDCODE_001',
+    weight: 100,
+    image: 'https://unsplash.com/photos/tUnhb_1qy4A'
+  },
+  {
+    _id: "203ce944f79e94-4f3b9e94c3",
+    name: 'Paracetamol',
+    code: 'MEDCODE_002',
+    weight: 200,
+    image: 'https://unsplash.com/photos/tUnhb_1qy4A'
+  },
+  {
+    _id: "203c3502b9ec4-5791-944f3c3",
+    name: 'Panadol',
+    code: 'MEDCODE_003',
+    weight: 300,
+    image: 'https://unsplash.com/photos/tUnhb_1qy4A'
+  }
+];
+
+module.exports = Medications;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const DronesDB = require('./db/drones.db');
+const MedicationsDB = require('./db/medications.db');
 const {
   BadRequestError,
   NotFoundError,
@@ -16,7 +17,7 @@ app.use(express.json());
 
 // DB Operations
 const getDrones = () => DronesDB;
-
+const getMeds = () => MedicationsDB;
 
 /**********************************************
  * ROUTE HANDLER
@@ -36,6 +37,18 @@ app.get('/api/drones', (req, res) => {
   try {
     const allDrones = getDrones();
     res.status(200).json({ data: allDrones });
+  } catch (error) {
+    throw new InternalServerError(error.message);
+  }
+});
+
+/**
+ * Get all medications
+ */
+app.get('/api/medications', (req, res) => {
+  try {
+    const allMeds = getMeds();
+    res.status(200).json({ data: allMeds });
   } catch (error) {
     throw new InternalServerError(error.message);
   }
